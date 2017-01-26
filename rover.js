9fa0f585a137608b123999b54a3ab985acd305f1
Oscar Guerrero <itsoscarg@icloud.com>
2017-01-26 12:04:05 -0500

    done

--- rover.js
+++ rover.js
@@ -22,17 +22,17 @@ var mrsRover = {
 };
 function doCommand(rover, com) {
   switch(com) {
-    case 'f':
+   case 'f':
       goForward(rover)
     break;
     case 'b':
       goBack(rover)
     break;
-    case 'r':
-      turnRover(rover, "E")
-    break;
     case 'l':
-      turnRover(rover, "W")
+      turnRover(rover, "l")
+    break;
+     case 'r':
+      turnRover(rover, 'r')
     break;
   }
 }
@@ -92,7 +92,7 @@ function goBack(rover){
   }
 }
 
-function turnRover(rover, newDirection){
+/* function turnRover(rover, newDirection){
   switch (newDirection) {
     case 'N':
     rover.direction = 'N'
@@ -108,14 +108,45 @@ function turnRover(rover, newDirection){
     break;
   }
 }
+*/
 
-var c = 'fffffff';
+//-----attempting to change direction NOT based on N,S,E,W
+
+function turnRover (rover, newDirection) {
+  switch (newDirection) {
+case 'r':
+    if (rover.direction = 'E'){
+      rover.direction = 'S'}
+    if (rover.direction = 'S'){
+      rover.direction = 'W'}
+    if (rover.direction = 'W'){
+      rover.direction = 'N'}
+    if (rover.direction = 'N'){
+      rover.direction = 'E'}
+      break;
+case 'l':
+    if (rover.direction = 'E'){
+      rover.direction = 'N'}
+    if (rover.direction = 'S'){
+       rover.direction = 'W'}
+    if (rover.direction = 'W'){
+      rover.direction = 'S'}
+    if (rover.direction = 'N'){
+      rover.direction = 'W'}
+      break;
+    }
+  }
+
+
+//--------------RUN THE PROGRAM------------
+
+var c = 'rffflfff';
 for (var i = 0; i < c.length; i++){
   (doCommand (myRover, c[i]));
   console.log("New Rover Position: [" + myRover.position[0] + ", " + myRover.position[1] + "]")
 }
 var d = 'ffffffff';
-for (var i = 0; i < c.length; i++){
+for (var i = 0; i < d.length; i++){
 (doCommand (mrsRover, c[i]));
 console.log("New Mrs. Rover Position: [" + mrsRover.position[0] + ", " + mrsRover.position[1] + "]")
 }
